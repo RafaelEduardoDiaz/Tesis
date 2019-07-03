def926e68d49b3d65ab93d2820698740d3d33aac
RafaelEduardoDiaz <rafael.diaz@usantotomas.edu.co>
2019-07-03 03:04:46 -0500

    ZIPHMM corregido

--- Bayes_fun.R
+++ Bayes_fun.R
@@ -1,8 +1,26 @@
+pois.HMM.confint <- function(mod, n, B = 25, alpha = 0.05){
+  m <- mod$m
+  lambda <- matrix(data=NA,nrow=B,ncol=m,dimnames=list(c(1:B),paste0("lambda",1:m)))
+  Gamma <- matrix(data=NA,nrow=B,ncol=m*m,dimnames=list(c(1:B),
+                  paste0("gamma",as.vector(outer(1:m, 1:m, paste, sep = "")))))
+  for(i in 1:B){
+    y <- pois.HMM.generate_sample(ns = n, mod = mod)
+    model <- pois.HMM.mle(y,m, mod$lambda , mod$gamma , stationary = TRUE)
+    lambda[i,] <- model$lambda
+    Gamma[i,] <- c(model$gamma)}
+  CI.lambda <- apply(X = lambda,MARGIN = 2, FUN = function(x)quantile(x,probs = c(alpha/2,1-alpha/2)))
+  CI.Gamma <- apply(X = Gamma,MARGIN = 2, FUN = function(x)quantile(x,probs = c(alpha/2,1-alpha/2)))
+  output <- rbind(cbind(mean = c(mod$gamma),t(CI.Gamma)),cbind(mean = mod$lambda,t(CI.lambda))) 
+  output <- as.data.frame(output)
+  output$Ancho <- output[,3] - output[,2]
+  return(output)
+}
+
 bayes.stadist <- function(model, m){
   df <- as.data.frame(summary(model)$summary)
   A <- matrix(df$mean[1:(m*m)],m,m, byrow = TRUE)
-  delta = 1/sum(A[!diag(diag(A))])*A[!diag(diag(A))]
-  return(delta)
+  delta <- rep(1,m)%*%solve(diag(m) - A + matrix(1,m,m))
+  return(c(delta))
 }
 
 bayes.viterbi <- function(x, model, m){
